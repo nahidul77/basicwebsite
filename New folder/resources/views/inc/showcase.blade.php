b435839e1cd30bab154506d1d68dabe4bb10eb06
Nahidul Islam <nahidul.mee@gmail.com>
2020-10-08 13:04:25 +0600

    new folder deleted

--- New folder/resources/views/inc/showcase.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="jumbotron text-center">
-    <div class="container">
-        <h1>Welcome to our website</h1>
-        <p class="lead">Welcome to our brand new laravel powered website. This site uses laravel version 8</p>
-    </div>
-</div>
--- New folder/resources/views/inc/sidebar.blade.php
+++
@@ -1,7 +0,0 @@
-@section('sidebar')
-
-    <div class="card">
-        <h3 class="card-header">Sidebar</h3>
-        <p class="card-body">This is the sidebar</p>
-    </div>  
-@show
